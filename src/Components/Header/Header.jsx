ff37bb247426672c476f883263cece6526811308
Adetutu Oluwasanmi <oluwasanmiadetutu@gmail.com>
2023-04-07 20:01:54 +0100

    added the header section

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import './Header.css'; 
-
+ 
 function Header() { 
     return(
      <div className='headercontainer'>    
-        Header
+
+        <div className='headername'>
+            <p className='name'>adetutuOluwasanmi</p>
+        </div>
+
+        <div className='headericons'>
+            <a href='https://github.com/adetutudeborah' target='_blank' className='socialicon'><i class="fa-brands fa-github"></i></a>
+            <a href='https://twitter.com/adetutuoluwa2' target='_blank' className='socialicon'><i className="fa-brands fa-twitter"></i></a>
+            <a href='https://www.linkedin.com/in/adetutu-oluwasanmi/' target='_blank' className='socialicon'><i class="fa-brands fa-linkedin"></i></a>
+            <a href='https://adetutu.hashnode.dev/' target='_blank' className='socialicon'><i class="fa-brands fa-hashnode"></i></a>
+        </div>
+        
     </div>    
     )
 }
