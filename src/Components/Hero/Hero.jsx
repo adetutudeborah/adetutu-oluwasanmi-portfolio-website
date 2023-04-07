45c875d424207f27bb6b1a6ceda0cfc726876721
Adetutu Oluwasanmi <oluwasanmiadetutu@gmail.com>
2023-04-07 21:09:45 +0100

    added hero section

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import './Hero.css'; 
-
+ 
 function Hero() { 
     return(
-     <div className='herocontainer'>    
-        Hero
+     <div className='herocontainer'>  
+
+         <div className='heroheader'>
+            <h1 className='herointro'>Nice to meet you! <br /> I’m <span className='heroname'>Adetutu Oluwasanmi</span></h1>
+            <p className='herodescription'> I'm a front-end developer who is passionate about building and improving web applications</p>
+         </div>
+
+         <div>
+            <a href="https://drive.google.com/file/d/1aVnOInwvvaX6NcImkzYhlzRSg094gZqY/view?usp=sharing" target='_blank' className='myresume'>My resume</a>
+         </div>
     </div>    
     )
 }
