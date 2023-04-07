6b4bb4504cd7fced510b3310dd44a836c1655607
Adetutu Oluwasanmi <oluwasanmiadetutu@gmail.com>
2023-04-08 00:21:19 +0100

    added the projects section

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,15 +1,96 @@
 import React from 'react';
-import './Projects.css'; 
+import './Projects.css';  
+import images from "../../assets/images";
 
 function Projects() { 
     return(
      <div className='projectscontainer'>    
 
-        <h2 className='projectheader'>Projects</h2>
+           <h2 className='projectheader'>Projects</h2>
 
-        <div>
+            <div className='projectlist row'>
 
-        </div>
+                    <div className='project col-6 mb-5'>
+                            <img src={images.designo} alt="designo-image" className='projectimg'/>
+                            <h3 className='projecttitle mt-4'>Designo website</h3>
+                            <div className='toolsused mt-3'>
+                                <span className='tool'>Html</span>
+                                <span className='tool'>Css</span>
+                                <span className='tool'>Leaflet Js</span>
+                                <span className='tool'>React</span>
+                            </div>
+
+                            <div className='viewproject mt-3'>
+                                <a href='https://designo-adetutudeborah.vercel.app/' target='_blank' className='view'>View Project</a>
+                                <a href='https://github.com/adetutudeborah/designo' target='_blank' className='view'>View Code</a>
+                            </div>
+                    </div>
+
+                    <div className='project col-6 mb-5'>
+                            <img src={images.joblisting} alt="job-listings-image" className='projectimg'/>
+                            <h3 className='projecttitle mt-4'>Job Listings with Filtering</h3>
+                            <div className='toolsused mt-3'>
+                                <span className='tool'>Html</span>
+                                <span className='tool'>Css</span>
+                                <span className='tool'>JavaScript</span>
+                                <span className='tool'>React</span>
+                            </div>
+
+                            <div className='viewproject mt-3'>
+                                <a href='https://job-listings-page-bf8t.vercel.app/' target='_blank' className='view'>View Project</a>
+                                <a href='https://github.com/adetutudeborah/job-listings-page' target='_blank' className='view'>View Code</a>
+                            </div>
+                    </div>
+
+                    <div className='project col-6 mb-5'>
+                            <img src={images.roots} alt="roots-image" className='projectimg'/>
+                            <h3 className='projecttitle mt-4'>Roots</h3>
+                            <div className='toolsused mt-3'>
+                                <span className='tool'>Html</span>
+                                <span className='tool'>Css</span>
+                                <span className='tool'>React</span>
+                                <span className='tool'>Firebase</span>
+                            </div>
+
+                            <div className='viewproject mt-3'>
+                                <a href='https://roots-landing-page.vercel.app/' target='_blank' className='view'>View Project</a>
+                                <a href='https://github.com/ADA-Software-Engineering-Program/Roots-landing-page' target='_blank' className='view'>View Code</a>
+                            </div>
+                    </div>
+
+                    <div className='project col-6 mb-5'>
+                            <img src={images.weatherapp} alt="weather-app-image" className='projectimg'/>
+                            <h3 className='projecttitle mt-4'>Weather App</h3>
+                            <div className='toolsused mt-3'>
+                                <span className='tool'>Html</span>
+                                <span className='tool'>Css</span>
+                                <span className='tool'>JavaScript</span>
+                                <span className='tool'>Weather API</span>
+                            </div>
+
+                            <div className='viewproject mt-3'>
+                                <a href='https://weather-app-adetutudeborah.vercel.app/' target='_blank' className='view'>View Project</a>
+                                <a href='https://github.com/adetutudeborah/weather-app' target='_blank' className='view'>View Code</a>
+                            </div>
+                    </div>
+
+                    <div className='project col-6 mb-5'>
+                            <img src={images.spefind} alt="spefind-image" className='projectimg'/>
+                            <h3 className='projecttitle mt-4'>Spefind</h3>
+                            <div className='toolsused mt-3'>
+                                <span className='tool'>Html</span>
+                                <span className='tool'>Css</span>
+                                <span className='tool'>React</span>
+                            </div>
+
+                            <div className='viewproject mt-3'>
+                                <a href='https://spefind.vercel.app/' target='_blank' className='view'>View Project</a>
+                                <a href='https://github.com/ADA-Software-Engineering-Program/spefind' target='_blank' className='view'>View Code</a>
+                            </div>
+                    </div>
+                    
+                    
+            </div>
     </div>    
     ) 
 }
--- src/assets/images.js
+++ src/assets/images.js
@@ -1,9 +1,17 @@
 import logo from "./react.svg"; 
-
+import designo from "./designo.jpg"
+import joblisting from "./job-listings.jpg";
+import roots from "./roots.png";
+import weatherapp from "./weatherapp.png"
+import spefind from "./spefind0.png"
 
 const images = {
   logo,
-
+  designo,
+  joblisting,
+  roots,
+  weatherapp,
+  spefind,
 };
 
 export default images;
