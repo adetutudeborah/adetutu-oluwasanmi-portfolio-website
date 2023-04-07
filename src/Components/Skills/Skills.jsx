6250aa33cd99df7b1cf27487dfe124c9aa7bf6b2
Adetutu Oluwasanmi <oluwasanmiadetutu@gmail.com>
2023-04-07 21:50:18 +0100

    added the skills section

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -3,10 +3,46 @@ import './Skills.css';
 
 function Skills() { 
     return(
-     <div className='skillscontainer'>    
-        skills
+     <div className='skillscontainer'> 
+
+         <h2 className='skillsheader'>Skills</h2>
+
+        <div className='skillssection row'>
+             
+                <div class="col-4 text-center mb-5">
+                    <span className='skillicon'><i class="fa-brands fa-html5"></i></span>
+                    <h3 class="skill">HTML</h3>
+                </div>
+
+                <div class="col-4 text-center mb-5">  
+                    <span className='skillicon'><i class="fa-brands fa-css3-alt"></i></span>
+                    <h3 class="skill">CSS</h3>
+                </div>
+
+                <div class="col-4 text-center mb-5">  
+                    <span className='skillicon'><i class="fa-brands fa-bootstrap"></i></span>
+                    <h3 class="skill">BootStrap</h3>
+                </div>
+
+                <div class="col-4 text-center mb-5">  
+                    <span className='skillicon'><i class="fa-brands fa-js"></i></span>
+                    <h3 class="skill">JavaScript</h3>
+                </div>
+
+                <div class="col-4 text-center mb-5">  
+                    <span className='skillicon'><i class="fa-brands fa-react"></i></span>
+                    <h3 class="skill">React</h3>
+                </div>
+
+                <div class="col-4 text-center mb-5">  
+                    <span className='skillicon'><i class="fa-brands fa-github"></i></span>
+                    <h3 class="skill">GitHub</h3>
+                </div>
+
+        </div>
+
     </div>    
-    )
+    ) 
 }
 
 export default Skills;
